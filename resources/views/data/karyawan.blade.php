71dc698f9efd7132e66aa94bd11abbcc5a4e37c7
rajifmahendra <rajifmahendra10@gmail.com>
2020-03-14 16:48:57 +0700

    workshop selesai

---
+++ resources/views/data/karyawan.blade.php
@@ -0,0 +1,5 @@
+@extends('utama.index')
+
+@section('content')
+    <h1>Data Karyawan PT Inovasi Teknologi</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/utama', 'UtamaController@index')->name('utama');
+Route::get('/karyawan', 'UtamaController@landing')->name('karyawan');
