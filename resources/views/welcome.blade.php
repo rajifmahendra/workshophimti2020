37af211cbea0401b63462b1e23fd2636c456e476
rajifmahendra <rajifmahendra10@gmail.com>
2020-03-14 11:51:14 +0700

    merubah tampilan welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    workshop
+                    rajif
                 </div>
 
                 <div class="links">
